529124031a1bfbae2cec7f9701c92add46615244
Mattias Pernhult <pernhultmattias@gmail.com>
2015-11-22 12:57:31 +0100

    Working example
    Removed console.log

--- extension.js
+++ extension.js
@@ -8,44 +8,54 @@ var Window = vscode.window;
 var Commands = vscode.commands;
 var Languages = vscode.languages;
 var issue = 'https://github.com/MattiasPernhult/vscode-todo/issues';
+var findFileExtension;
+var statusBarItem;
+var choosenLanguage;
 
 var openBrowser = Commands.registerCommand('extension.openBrowser', function() {
 	opener(issue);
 });
 
 var openQuickPick = Commands.registerCommand('extension.openQuickPick', function() {
-	if (resultTodo === undefined) {
+	if (findFileExtension === undefined) {
+		Window.showInformationMessage('You must choose a language first, use the command palette.');
 		return;
 	}
-	var resultTodoName = [];
-	for (var index in resultTodo) {
-		for (var i = 0; i < resultTodo[index].length; i++) {
-			var todo = resultTodo[index][i];
-			resultTodoName.push(todo.name);
+	findFiles(findFileExtension, function() {
+		if (resultTodo === undefined) {
+			return;
 		}
-	}
-	Window.showQuickPick(resultTodoName, {}).then(function(response) {
-		var nameSplit = new String(response).split(' ');
-		var file = 'file://' + nameSplit[0];
-		var fileUri = vscode.Uri.parse(file);
-		Workspace.openTextDocument(fileUri).then(function(textDocument) {
-			Window.showTextDocument(textDocument, vscode.ViewColumn.One).then(function(textEditor) {
-				var line = Number(nameSplit[1].split(':')[0]) - 1;
-				var resultObjects = resultTodo[nameSplit[0]];
-				var startPos;
-				var endPos;
-				for (var i = 0; i < resultObjects.length; i++) {
-					var object = resultObjects[i];
-					if (object.line === line) {
-						startPos = new vscode.Position(object.line, 0);
-						endPos = new vscode.Position(object.line, object.lineLength);
-						break;
+		var resultTodoName = [];
+		for (var index in resultTodo) {
+			for (var i = 0; i < resultTodo[index].length; i++) {
+				var todo = resultTodo[index][i];
+				resultTodoName.push(todo.name);
+			}
+		}
+		Window.showQuickPick(resultTodoName, {}).then(function(response) {
+			var nameSplit = new String(response).split(' ');
+			var file = 'file://' + nameSplit[0];
+			var fileUri = vscode.Uri.parse(file);
+			Workspace.openTextDocument(fileUri).then(function(textDocument) {
+				Window.showTextDocument(textDocument, vscode.ViewColumn.One).then(function(textEditor) {
+					var line = Number(nameSplit[1].split(':')[0]) - 1;
+					var resultObjects = resultTodo[nameSplit[0]];
+					var startPos;
+					var endPos;
+					for (var i = 0; i < resultObjects.length; i++) {
+						var object = resultObjects[i];
+						if (object.line === line) {
+							startPos = new vscode.Position(object.line, 0);
+							endPos = new vscode.Position(object.line, object.lineLength);
+							break;
+						}
 					}
-				}
-				Window.activeTextEditor.selection = new vscode.Selection(startPos, endPos);
+					Window.activeTextEditor.selection = new vscode.Selection(startPos, endPos);
+				});
 			});
 		});
 	});
+	
 });
 
 // this method is called when the extension is activated
@@ -54,13 +64,9 @@ function activate(context) {
 	// this line of code will only be executed once when the extension is activated
 	console.log('Congratulations, your extension "vscode-todo" is now active!');
 
-	var statusBarItem = Window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-	statusBarItem.text = 'TODO:s';
-	statusBarItem.tooltip = 'Show TODO:s';
-	statusBarItem.command = 'extension.openQuickPick';
-	statusBarItem.color = '#FFFFFF';
-	console.log(statusBarItem);
-	statusBarItem.show();
+	if (statusBarItem === undefined) {
+		createStatusBarItem();	
+	}
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
@@ -69,8 +75,8 @@ function activate(context) {
 		var languageChoice = ['Go', 'Javascript', 'PHP', 'Coffeescript', 'C', 'C++', 'C#', 'Objective-C', 'Python', 'Ruby', 'Swift', 'Typescript', 'VisualBasic'];
 
 		Window.showQuickPick(languageChoice, {}).then(function(language) {
-			var findFiles = getFileExtension(language);
-			if (findFiles === null) {
+			var findFilesExt = getFileExtension(language);
+			if (findFilesExt === null) {
 				Window.showErrorMessage('The choosen language "' + language + '" didn\'t match any file extensions, please create an issue', 'Create issue').then(function(choice) {
 					if (choice === 'Create issue') {
 						Commands.executeCommand('extension.openBrowser', function() {
@@ -79,20 +85,40 @@ function activate(context) {
 					}
 				});
 			} else {
-				Workspace.findFiles(findFiles, '', 1000).then(function(files) {
-					doWork(files, function(result) {
-						if (result.length == 0) {
-							Window.showInformationMessage('There is no TODO:s');
-						} else {
-							resultTodo = result;
-						}
-					});
-				});
+				choosenLanguage = language; 
+				if (statusBarItem === undefined) {
+					createStatusBarItem();
+				}
+				statusBarItem.text = 'TODO:s for ' + language;
+				statusBarItem.tooltip = 'Show TODO:s for ' + language;
+				findFileExtension = findFilesExt;
 			}
 		});
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function createStatusBarItem() {
+	statusBarItem = Window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+	statusBarItem.text = 'TODO:s';
+	statusBarItem.tooltip = 'Show TODO:s';
+	statusBarItem.command = 'extension.openQuickPick';
+	statusBarItem.color = '#FFFFFF';
+	statusBarItem.show();
+}
+
+function findFiles(findFilesExtension, done) {
+	Workspace.findFiles(findFilesExtension, '', 1000).then(function(files) {
+		doWork(files, function(err, result) {
+			if (err) {
+				resultTodo = undefined;
+			} else {
+				resultTodo = result;	
+			}
+			done();
+		});
+	});
 }
 
 function getFileExtension(language) {
@@ -121,30 +147,35 @@ function getObject() {
 function doWork(files, done) {
 	var message = {};
 	var times = 0;
-	for (var i = 0; i < files.length; i++) {
-		Workspace.openTextDocument(files[i]).then(function(file) {
-			var uriString = String(file._uri);
-			var pathWithoutFile = uriString.substring(7, uriString.length);
-			for (var line = 0; line < file._lines.length; line++) {
-				var textLine = String(file._lines[line]);
-				if (textLine.includes('TODO:')) {
-					if (!message.hasOwnProperty(textLine)) {
-						message[pathWithoutFile] = [];
+	if (files.length === 0) {
+		Window.showInformationMessage('There is no ' + choosenLanguage + ' files in the open project.');
+		done({message: 'no files'}, message);
+	} else {
+		for (var i = 0; i < files.length; i++) {
+			Workspace.openTextDocument(files[i]).then(function(file) {
+				var uriString = String(file._uri);
+				var pathWithoutFile = uriString.substring(7, uriString.length);
+				for (var line = 0; line < file._lines.length; line++) {
+					var textLine = String(file._lines[line]);
+					if (textLine.includes('TODO:')) {
+						if (!message.hasOwnProperty(textLine)) {
+							message[pathWithoutFile] = [];
+						}
+						var object = getObject();
+						object.name = pathWithoutFile + ' ' + (line + 1) + ':' + (textLine.indexOf('TODO:') + 1);
+						object.fileName = pathWithoutFile;
+						object.line = line;
+						object.lineLength = textLine.length;
+						message[pathWithoutFile].push(object);
 					}
-					var object = getObject();
-					object.name = pathWithoutFile + ' ' + (line + 1) + ':' + (textLine.indexOf('TODO:') + 1);
-					object.fileName = pathWithoutFile;
-					object.line = line;
-					object.lineLength = textLine.length;
-					message[pathWithoutFile].push(object);
 				}
-			}
-		}).then(function() {
-			times++;
-			if (times === files.length) {
-				return done(message);
-			}
-		});
+			}).then(function() {
+				times++;
+				if (times === files.length) {
+					return done(null, message);
+				}
+			});
+		}
 	}
 }
 
